d347033989f3881699439e68c703b70eccd90a63
Bhuvan Teja Kudupudi <bhuvanteja.kudupudi@gmail.com>
2026-01-22 14:08:46 -0500

    added analytics and firebase hosting

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "874193284"
+    serviceWorkerVersion: "3475304396"
   }
 });
