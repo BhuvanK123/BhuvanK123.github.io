167afc17feba564b05cb5ac30fd9e0eedb311f71
Bhuvan Teja Kudupudi <bhuvanteja.kudupudi@gmail.com>
2026-02-03 21:03:45 -0500

    added floating action button and animation around globe

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "95182635"
+    serviceWorkerVersion: "3780644306"
   }
 });
